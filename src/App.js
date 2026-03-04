26b0cb95abb0a1280b692306179b24828adce6c4
Chaitanya Sethi <chaitanyasethi@Chaitanyas-MacBook-Air.local>
2026-03-04 06:22:08 +0530

    portfolio updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useMemo, useCallback } from "react";
+import { useState, useEffect, useRef} from "react";
 import * as THREE from "three";
 import { motion, AnimatePresence, useMotionValue, useSpring, useTransform } from "framer-motion";
 
